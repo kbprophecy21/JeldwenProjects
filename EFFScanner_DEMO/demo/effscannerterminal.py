b8f971f9a7e4414de276cfb20fe2fa557d12ddc9
Kyle <djprophecy21@gmail.com>
2025-06-15 15:15:39 -0500

    got it working for all tickets except for sales department. need to fix that later not import right now. I have extracted the data into indivisual variables.

--- EFFScanner_DEMO/demo/effscannerterminal.py
+++ EFFScanner_DEMO/demo/effscannerterminal.py
@@ -6,7 +6,7 @@ import os
 
 folder_path = r"c:\Users\14704\OneDrive\Desktop\All Projects\Wild_Projects\Jeldwen_Projects\JeldwenProjects\Data\LIS_Files"
 
-print("Enter the Schedule Batch ID: ")   #example data: 0609250160002
+print("Enter the Schedule Batch ID: ")   # example data: 0602250010001   ,  060625ALR0005001 , 0609250160002  ,  061025BFS0010001
 
 batch_id = input().strip()
 
@@ -35,11 +35,19 @@ for file in os.listdir(folder_path):
                 search_range = [field.strip('"') for field in fields[1:5]]
 
                 if group_press_A in search_range and group_press_B in search_range:
-                    print(f"✅ Match found for '{group_press_A}' or '{group_press_B}'")
                     print("Matched Line:")
                     ticket_data = line.strip()
-                    print(ticket_data)
-                    # Here you can add further processing of the matched line if needed
+                    
+                    fields = [field.strip('"') for field in ticket_data.split(",")]
+                    door_quanity = fields[5]
+                    door_size = fields[7]
+                    door_frameCode = fields[8].strip().split()[0]
+
+                    
+                    print(f"Door Quantity: {door_quanity}")
+                    print(f"Door Size: {door_size}")
+                    print(f"Door Frame Code: {door_frameCode}")
+                    
                     found = True
                     break
 
@@ -49,5 +57,3 @@ for file in os.listdir(folder_path):
         break  # Stop after finding and processing the correct file
 else:
     print(f"❌ File '{file_name}' not found in {folder_path}")
-
-print(f"Group Press: {group_press_A}-{group_press_B}")
